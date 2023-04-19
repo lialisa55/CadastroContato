074456a70a182b7e1ed337d6ea543f497d2b2db1
unknown <nathalia.mota@etec.sp.gov.br>
2023-04-19 20:16:22 -0300

    added control class

--- AgendaContatos/Controle.cs
+++ AgendaContatos/Controle.cs
@@ -9,5 +9,27 @@ namespace AgendaContatos
 {
     class Controle
     {
+        Conexao c = new Conexao();
+
+        public string Cadastrar(Contato cont)
+        {
+            try
+            {
+                string sql  = "INSERT INTO tbcontato (nome, telefone, celular, email) " + 
+                    "VALUES ('" + cont.Nome + "', '" + cont.Telefone + 
+                    "', '" + cont.Celular + "', '" + cont.Email + "')";
+
+                MySqlCommand cmd = new MySqlCommand(sql, c.con);
+
+                c.conectar();
+                cmd.ExecuteNonQuery();
+                c.desconectar();
+                return ("Cadastro realizado com sucesso.");
+            }
+            catch (MySqlException e)
+            {
+                return (e.ToString());
+            }
+        }
     }
 }
